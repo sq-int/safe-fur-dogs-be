cdbfb3113b7145cc3a56eba7d37c2a1081066c5a
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-05-07 13:06:27 -0500

    rate limit added

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const server = express();
 
@@ -9,10 +10,18 @@ const server = express();
 const foodRouter = require('./food/router');
 const aRouter = require('./acp/router');
 
+/* configure rate limit */
+const limiter = rateLimit({
+    windowMs: 20 * 60 * 1000,
+    max: 20,
+    message: 'Slow down there, cowboy. You have sent too many requests. We limit searches currently as Safe Fur Dogs is a free service. Please wait some time before submitting another request, or consider using the coffee icon on the website to buy us a coffee so we can increase our limits.'
+});
+
 /* middleware */
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(limiter);
 
 /* routes */
 server.use('/api/food', foodRouter);
