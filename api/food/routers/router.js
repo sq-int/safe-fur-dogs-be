30cbc7e1501726d786b841f3e58a57f3faf073ab
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-09-21 16:35:09 -0500

    GET a list of all food items in the database added.

--- api/food/routers/router.js
+++ api/food/routers/router.js
@@ -7,6 +7,15 @@ const router = express.Router();
 // Food model
 const Food = require("../models/Food.js");
 
+// GET a list of all food
+router.get("/", (req, res) => {
+  Food.find()
+    .then((data) => res.json(data))
+    .catch((err) =>
+      res.json({ error: "The list of food could not be retrieved." })
+    );
+});
+
 // POST to add a new food
 router.post("/", (req, res) => {
   const foodToAdd = req.body;
